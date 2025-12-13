1b9832131f15ec8f5f90095eb60e426d9e0a17c7
soma matsumoto <souma67jiji@icloud.com>
2025-12-13 23:24:25 +0900

    feat: サブスク画面/固定費画面のルート設定実装

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\SubscriptionController;
+use App\Http\Controllers\FixedCostController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -10,6 +12,22 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/subscriptions', [SubscriptionController::class, 'index'])
+        ->name('subscriptions.index');
+    Route::get('/subscriptions/create', [SubscriptionController::class, 'create'])
+        ->name('subscriptions.create');
+    Route::post('/subscriptions/store', [SubscriptionController::class, 'store'])
+        ->name('subscriptions.store');
+});
+
+Route::middleware('auth')->group(function () {
+    Route::get('/fixedCosts', [FixedCostController::class, 'index'])
+        ->name('fixed-costs.index');
+    Route::get('/fixedCosts/create', [FixedCostController::class, 'create'])
+        ->name('fixed-costs.create');
+});
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
