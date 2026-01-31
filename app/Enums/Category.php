8026df96b21f4657db3745cb130c5e425e0e1f65
soma matsumoto <souma67jiji@icloud.com>
2026-01-31 21:18:55 +0900

    feat: カテゴリーEnum定義 - フィルタリング実装に向けてカテゴリーをEnum化

---
+++ app/Enums/Category.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Enums;
+
+enum Category: string
+{
+    case LIVING = 'living';
+    case ENTERTAINMENT = 'entertainment';
+    case PET = 'pet';
+    case INSURANCE = 'insurance';
+    case STUDY = 'study';
+
+    public function label(): string
+    {
+        return match($this) {
+            self::LIVING => '生活費',
+            self::ENTERTAINMENT => 'エンタメ',
+            self::PET => 'ペット',
+            self::INSURANCE => '保険',
+            self::STUDY => '勉強',
+        };
+    }
+
+    public function CategoryColor(): string
+    {
+        return match($this) {
+             self::LIVING => 'text-blue-700',
+             self::ENTERTAINMENT => 'text-purple-700',
+             self::PET => 'text-orange-700',
+             self::INSURANCE => 'text-gray-700',
+             self::STUDY => 'text-green-700',
+        };
+    }
+
+    public static function option(): array
+    {
+        return array_map(fn($case) => [
+            'value' => $case->value,
+            'label' => $case->label()
+        ], self::cases());
+    }
+}
