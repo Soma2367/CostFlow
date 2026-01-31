ca674a966385cd1d53ea451bfae9707f7f32d0ba
soma matsumoto <souma67jiji@icloud.com>
2026-01-31 21:50:55 +0900

    refactor: AdminControllerにサービス導入

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -5,12 +5,35 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Income;
 use Illuminate\Support\Facades\Auth;
+use App\Services\AdminService;
 
 class AdminController extends Controller
 {
+    protected $adminService;
+
+    public function __construct(AdminService $adminService)
+    {
+        $this->adminService = $adminService;
+    }
+
     public function index()
     {
-        $income = Income::where('user_id', Auth::id())->get();
-        return view('admins.index', compact('income'));
+        $income = $this->adminService->getIncome(Auth::id());
+        $sumSubsc = $this->adminService->sumOfSubscription(Auth::id());
+        $sumFixedCost = $this->adminService->sumOfFixedCost(Auth::id());
+        $totalExpense = $sumSubsc + $sumFixedCost;
+        $subscItems = $this->adminService->subScriptionItem(Auth::id());
+        $fixedCostItems = $this->adminService->fixedCostItem(Auth::id());
+        $adminData = $this->adminService->adminChart(Auth::id());
+
+        return view('admins.index', compact(
+            'income',
+            'sumSubsc',
+            'sumFixedCost',
+            'totalExpense',
+            'subscItems',
+            'fixedCostItems',
+            'adminData'
+        ));
     }
 }
