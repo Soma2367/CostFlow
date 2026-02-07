be7785f4a85304ca50ea6eccd42c18b73d6041ab
soma matsumoto <souma67jiji@icloud.com>
2026-02-07 23:21:06 +0900

    fix: サブスク画面のフォームステータス・カテゴリ選択処理改善 - データ取得処理変更

--- resources/views/subscriptions/create.blade.php
+++ resources/views/subscriptions/create.blade.php
@@ -73,8 +73,9 @@
                                     required
                                     class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-blue-500 focus:bg-white focus:ring-2 focus:ring-blue-200 text-base outline-none text-gray-700 py-2 px-3 leading-8 transition-colors duration-200 ease-in-out"
                                 >
+                                    <option value="" disabled selected>選択してください</option>
                                     @foreach ($categories as $category)
-                                        <option value="{{ $category->value }}">{{ $category->label() }}</option>
+                                        <option value="{{ $category['value'] }}">{{ $category['label'] }}</option>
                                     @endforeach
                                 </select>
                             </div>
@@ -91,8 +92,8 @@
                                     required
                                     class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-blue-500 focus:bg-white focus:ring-2 focus:ring-blue-200 text-base outline-none text-gray-700 py-2 px-3 leading-8 transition-colors duration-200 ease-in-out"
                                 >
-                                    @foreach ($statuese as $status)
-                                        <option value="{{ $status->value }}">{{ $status->label() }}</option>
+                                    @foreach ($statuses as $status)
+                                        <option value="{{ $status['value'] }}">{{ $status['label'] }}</option>
                                     @endforeach
                                 </select>
                             </div>
--- resources/views/subscriptions/edit.blade.php
+++ resources/views/subscriptions/edit.blade.php
@@ -79,9 +79,10 @@
                                 <select name="category" id="category" required
                                 class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-blue-500 focus:bg-white focus:ring-2 focus:ring-blue-200 text-base outline-none text-gray-700 py-2 px-3 leading-8 transition-colors duration-200 ease-in-out">
                                      @foreach($categories as $category)
+                                        <option value="">選択してください</option>
                                         <option value="{{ $category->value }}"
                                             {{ old('category', $subscription->category->value) === $category->value ? 'selected' : '' }}>
-                                            {{ $category->label() }}
+                                            {{ $category['label'] }}
                                         </option>
                                     @endforeach
                                 </select>
