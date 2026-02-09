9d149f43a021db63bb49f0e9c18f1c1b5dba626a
soma matsumoto <souma67jiji@icloud.com>
2026-02-09 21:47:26 +0900

    refactor: LPのルートにname('welcome')を定義

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,7 @@ use Illuminate\Support\Facades\Route;
 // Public Routes
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('welcome');
 
 // Authenticated & Verified Routes
 Route::middleware(['auth', 'verified'])->group(function () {
