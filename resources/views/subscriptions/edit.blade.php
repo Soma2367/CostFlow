88b8317966a4c6a6e4c6943245c90136ce6b256c
soma matsumoto <souma67jiji@icloud.com>
2026-02-10 21:04:58 +0900

    fix: ステータス表示記法修正

--- resources/views/subscriptions/edit.blade.php
+++ resources/views/subscriptions/edit.blade.php
@@ -80,8 +80,8 @@
                                 class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-blue-500 focus:bg-white focus:ring-2 focus:ring-blue-200 text-base outline-none text-gray-700 py-2 px-3 leading-8 transition-colors duration-200 ease-in-out">
                                      @foreach($categories as $category)
                                         <option value="">選択してください</option>
-                                        <option value="{{ $category->value }}"
-                                            {{ old('category', $subscription->category->value) === $category->value ? 'selected' : '' }}>
+                                        <option value="{{ $category['value'] }}"
+                                            {{ old('category', $subscription->category->value) === $category['value'] ? 'selected' : '' }}>
                                             {{ $category['label'] }}
                                         </option>
                                     @endforeach
@@ -100,9 +100,10 @@
                                     required
                                     class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-blue-500 focus:bg-white focus:ring-2 focus:ring-blue-200 text-base outline-none text-gray-700 py-2 px-3 leading-8 transition-colors duration-200 ease-in-out"
                                 >
-                                    @foreach ($statuese as $status)
-                                        <option value="{{ $status->value }}">
-                                            {{ $status->label() }}
+                                    @foreach($statuses as $status)
+                                        <option value="{{ $status['value'] }}"
+                                            {{ old('status', $subscription->status->value) === $status['value'] ? 'selected' : '' }}>
+                                            {{ $status['label'] }}
                                         </option>
                                     @endforeach
                                 </select>
